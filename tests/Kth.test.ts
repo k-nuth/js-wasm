6ec604b21b438f2b4cfc2485a6d5274e375cc58a
Fernando Pelliccioni <fpelliccioni@gmail.com>
2024-12-16 16:39:57 +0100

    feat: improve build information

--- tests/Kth.test.ts
+++ tests/Kth.test.ts
@@ -5,35 +5,63 @@
 import { Kth } from '..';
 
 describe('Kth', () => {
+	describe('getLibconfig', () => {
+		it('should return a properly structured LibConfig object with valid default values', async () => {
+			const config = Kth.getLibconfig();
 
-    describe('getLibconfig', () => {
-        it('should return the correct configuration', () => {
-            const config = Kth.getLibconfig();
-            expect(config).toBeDefined();
-            expect(config.currency).toEqual('bitcoin_cash');
-            expect(config.dbReadonly).toEqual(false);
-            expect(config.debugMode).toEqual(false);
-            expect(config.logLibrary).toEqual('spdlog');
-            expect(config.mempool).toEqual(false);
-            expect(config.microarchitectureId).toEqual('');
-            expect(config.useLibmdbx).toEqual(false);
-            expect(config.version).toEqual('0.54.0');    //C-API version
+			// General Assertions
+			expect(config).toBeDefined();
 
-            expect(config.architecture).toEqual('WASM');
-            expect(config.osName).toEqual('WebAssembly Host');
-            expect(config.compilerName).toEqual('Emscripten');
-            expect(config.compilerVersion).toEqual('Clang 20.0.0git (https:/github.com/llvm/llvm-project 7543d09b852695187d08aa5d56d50016fea8f706)');
-            const capiBuildTimestamp = new Date(config.capiBuildTimestamp);
-            expect(capiBuildTimestamp.getFullYear()).toEqual(2024);
-            expect(capiBuildTimestamp.getMonth()).toEqual(11); // January is 0
-            expect(capiBuildTimestamp.getDate()).toEqual(13);
+			// General System Information
+			expect(config.architecture).toEqual('WASM');
+			expect(config.osName).toEqual('WebAssembly Host');
+			expect(config.endianness).toEqual('Little-endian');
 
-            expect(config.emscriptenVersion).toEqual('3.1.66');
-            const wasmBuildTimestamp = new Date(config.wasmBuildTimestamp);
-            expect(wasmBuildTimestamp.getFullYear()).toEqual(2024);
-            expect(wasmBuildTimestamp.getMonth()).toEqual(11); // January is 0
-            expect(wasmBuildTimestamp.getDate()).toEqual(13);
-        });
-    });
+			// Compiler and Build Information
+			expect(config.compilerName).toEqual('Emscripten');
+			expect(config.compilerVersion).toEqual('Clang 20.0.0git (https:/github.com/llvm/llvm-project 7543d09b852695187d08aa5d56d50016fea8f706)');
+			expect(config.optimizationLevel).toEqual('-O2');
 
+			const cppapiBuildTimestamp = new Date(config.cppapiBuildTimestamp);
+			expect(cppapiBuildTimestamp.getFullYear()).toEqual(2024);
+			expect(cppapiBuildTimestamp.getMonth()).toEqual(11); // January is 0
+			expect(cppapiBuildTimestamp.getDate()).toEqual(16);
+
+			const capiBuildTimestamp = new Date(config.capiBuildTimestamp);
+			expect(capiBuildTimestamp.getFullYear()).toEqual(2024);
+			expect(capiBuildTimestamp.getMonth()).toEqual(11); // January is 0
+			expect(capiBuildTimestamp.getDate()).toEqual(16);
+
+			const wasmBuildTimestamp = new Date(config.wasmBuildTimestamp);
+			expect(wasmBuildTimestamp.getFullYear()).toEqual(2024);
+			expect(wasmBuildTimestamp.getMonth()).toEqual(11); // January is 0
+			expect(wasmBuildTimestamp.getDate()).toEqual(16);
+
+			// Type Sizes
+			expect(config.typeSizesInt).toEqual(4);
+			expect(config.typeSizesLong).toEqual(4);
+			expect(config.typeSizesPointer).toEqual(4);
+
+			// Library and Version Information
+			expect(config.cppapiVersion).toEqual('0.50.0');
+			expect(config.capiVersion).toEqual('0.56.0'); // C-API version
+			expect(config.wasmLibraryVersion).toEqual('2.3.0');
+			expect(config.logLibrary).toEqual('spdlog');
+			expect(config.useLibmdbx).toEqual(false);
+
+			// Specific Platform Information
+			expect(config.microarchitectureId).toEqual('');
+			expect(config.marchNames).toEqual('');
+
+			// Application Configuration
+			expect(config.currency).toEqual('bitcoin_cash');
+			expect(config.currencySymbol).toEqual('BCH');
+			expect(config.mempool).toEqual(false);
+			expect(config.dbReadonly).toEqual(false);
+			expect(config.debugMode).toEqual(false);
+
+			// Emscripten Version
+			expect(config.emscriptenVersion).toEqual('3.1.66');
+		});
+	});
 });
--- types/Kth.d.ts
+++ types/Kth.d.ts
@@ -1,21 +1,30 @@
 export declare class Kth {
     static getLibconfig(): {
-        logLibrary: "boost" | "spdlog" | "binlog";
-        useLibmdbx: boolean;
-        version: string;
-        microarchitectureId: string;
-        currency: "bitcoin" | "bitcoin_cash" | "litecoin";
-        mempool: boolean;
-        dbReadonly: boolean;
-        debugMode: boolean;
         architecture: string;
         osName: string;
+        endianness: string;
         compilerName: string;
         compilerVersion: string;
         optimizationLevel: string;
+        cppapiBuildTimestamp: number;
         capiBuildTimestamp: number;
-        emscriptenVersion: string;
         wasmBuildTimestamp: number;
+        typeSizesInt: number;
+        typeSizesLong: number;
+        typeSizesPointer: number;
+        cppapiVersion: string;
+        capiVersion: string;
+        wasmLibraryVersion: string;
+        logLibrary: "boost" | "spdlog" | "binlog";
+        useLibmdbx: boolean;
+        microarchitectureId: string;
+        marchNames: string;
+        currency: "bitcoin" | "bitcoin_cash" | "litecoin";
+        currencySymbol: string;
+        mempool: boolean;
+        dbReadonly: boolean;
+        debugMode: boolean;
+        emscriptenVersion: string;
     };
 }
 export default Kth;
